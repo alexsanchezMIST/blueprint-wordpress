41e631b3420f0c0d428f1916189e21032614da53
ansolvera <38958444+ansolvera@users.noreply.github.com>
2022-12-19 00:06:25 -0800

    Debug

--- blocks/services/services.php
+++ blocks/services/services.php
@@ -1,16 +1,21 @@
+<?php if( have_rows('services')) : ?>
 <section id="services" class="section">
     <div class="container">
         <div class="card--wrapper">
-            <a href="<?php site_url(); ?>/services#<?php the_field('services_heading'); ?>" class="card">
-                <?php if (get_field('services_icon')): ?>
-                    <img src="<?php the_field('services_icon'); ?>" alt="Icon" class="card--icon" />
+            <?php while( have_rows('services')) : the_row(); ?>
+            <a href="<?php site_url(); ?>/services#<?php the_sub_field('services_heading'); ?>" class="card">
+                <?php if (get_sub_field('services_icon')): ?>
+                    <img src="<?php the_sub_field('services_icon'); ?>" alt="Icon" class="card--icon" />
                 <?php endif; ?>
-                    <h5 class="card--heading"><?php the_field('services_heading'); ?></h5>
-                <?php if (get_field('services_description')): ?>
-                    <div class="card--content"><?php the_field('services_description'); ?></div>
+                    <h5 class="card--heading"><?php the_sub_field('services_heading'); ?></h5>
+                <?php if (get_sub_field('services_description')): ?>
+                    <div class="card--content"><?php the_sub_field('services_description'); ?></div>
                 <?php endif; ?>
                 <p class="card--link">Learn more →</p>
             </a>
+            <?php endwhile; ?>
         </div>
     </div>
 </section>
+<?php wp_reset_postdata(); ?>
+<?php endif;?>
