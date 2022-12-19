f84de9bf139e8095d6e9abb36d1c666cea633fa6
ansolvera <38958444+ansolvera@users.noreply.github.com>
2022-12-18 23:59:31 -0800

    Edit services block

--- blocks/services/services.php
+++ blocks/services/services.php
@@ -1,28 +1,16 @@
-<?php
-
-$services = new WP_Query(array(
-    'numberposts' => -1,
-    'post_type' => 'service',
-    'post_status' => 'published',
-));
-?>
-
 <section id="services" class="section">
     <div class="container">
-        <?php if($services ->  have_posts()): ?>
-            <div class="card--wrapper">
-                <?php while ($services -> have_posts()) : $services -> the_post(); ?>
-                    <a href="<?php site_url(); ?>/services#<?php the_title(); ?>" class="card">
-                        <?php if (get_field('services_icon')): ?>
-                            <img src="<?php the_field('services_icon'); ?>" alt="Icon" class="card--icon" />
-                        <?php endif; ?>
-                        <h5 class="card--heading"><?php the_title(); ?></h5>
-                        <div class="card--content"><?php the_excerpt(); ?></div>
-                        <p class="card--link">Learn more →</p>
-                    </a>
-                <?php endwhile; ?>
-            </div>
-        <?php wp_reset_postdata(); ?>
-        <?php endif; ?>
+        <div class="card--wrapper">
+            <a href="<?php site_url(); ?>/services#<?php the_field('services_heading'); ?>" class="card">
+                <?php if (get_field('services_icon')): ?>
+                    <img src="<?php the_field('services_icon'); ?>" alt="Icon" class="card--icon" />
+                <?php endif; ?>
+                    <h5 class="card--heading"><?php the_field('services_heading'); ?></h5>
+                <?php if (get_field('services_description')): ?>
+                    <div class="card--content"><?php the_field('services_description'); ?></div>
+                <?php endif; ?>
+                <p class="card--link">Learn more →</p>
+            </a>
+        </div>
     </div>
 </section>
